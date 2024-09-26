a2f353b3cb0c2816e51113512d31943b619062f9
Jamanul <jk.skb.25@gmail.com>
2024-09-26 12:55:47 +0600

    setting up next auth done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from 'next-auth/providers/credentials';
 import NextAuth from "next-auth/next";
+import { connectDb } from "@/app/lib/ConnectDb";
+const bcrypt = require('bcrypt');
 
 export const authOptions:NextAuthOptions ={
     secret: "",
@@ -18,6 +20,25 @@ export const authOptions:NextAuthOptions ={
                 const { email, password } = credentials as {
                     email: string;
                     password: string;
+                  };
+                if(!email || !password){
+                    return null
+                }
+                const db = await connectDb()
+                const usersCollection = db.collection('users')
+                const currentUser = await usersCollection.findOne({email:email})
+                if(!currentUser){
+                    return null
+                }
+                const passwordMatch = bcrypt.compareSync(password, currentUser.password);
+                if(!passwordMatch){
+                    return null
+                }
+
+                return {
+                    id: currentUser._id.toString(),
+                    email: currentUser.email,
+                    name: currentUser.name,
                   };
             }
         })
