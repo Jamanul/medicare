da16d241f88d2906d9a764add5babb888e72fd23
Jamanul <jk.skb.25@gmail.com>
2024-09-27 18:43:29 +0600

    setting up next auth social login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,62 @@
 import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from 'next-auth/providers/credentials';
+import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth/next";
 import { connectDb } from "@/app/lib/ConnectDb";
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
-export const authOptions:NextAuthOptions ={
-    secret: "",
-    session:{
-        strategy: 'jwt',
-        maxAge: 24*60*60*30
-    },
-    providers :[
-        CredentialsProvider({
-            credentials: {
-                email: {},
-                password:{}
-            },
-            async authorize(credentials){
-                const { email, password } = credentials as {
-                    email: string;
-                    password: string;
-                  };
-                if(!email || !password){
-                    return null
-                }
-                const db = await connectDb()
-                const usersCollection = db.collection('users')
-                const currentUser = await usersCollection.findOne({email:email})
-                if(!currentUser){
-                    return null
-                }
-                const passwordMatch = bcrypt.compareSync(password, currentUser.password);
-                if(!passwordMatch){
-                    return null
-                }
+export const authOptions: NextAuthOptions = {
+  secret: "",
+  session: {
+    strategy: "jwt",
+    maxAge: 24 * 60 * 60 * 30,
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        if (!email || !password) {
+          return null;
+        }
+        const db = await connectDb();
+        const usersCollection = db.collection("users");
+        const currentUser = await usersCollection.findOne({ email: email });
+        if (!currentUser) {
+          return null;
+        }
+        const passwordMatch = bcrypt.compareSync(
+          password,
+          currentUser.password
+        );
+        if (!passwordMatch) {
+          return null;
+        }
+        return {
+          id: currentUser._id.toString(),
+          email: currentUser.email,
+          name: currentUser.name,
+        };
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET as string,
+    }),
+  ],
+};
 
-                return {
-                    id: currentUser._id.toString(),
-                    email: currentUser.email,
-                    name: currentUser.name,
-                  };
-            }
-        })
-    ]
-}
+const handler = NextAuth(authOptions);
 
-
-const handler =NextAuth(authOptions)
-
-export {handler as GET ,handler as POST}
+export { handler as GET, handler as POST };
